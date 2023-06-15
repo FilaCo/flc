0c70aed4a53dde725ab700aa864419ca535dc78f
FilaCo <awesome.chekushka@gmail.com>
2023-06-15 23:13:32 +0500

    Storybook added

---
+++ web/src/lib/components/atoms/Button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { SvelteComponent } from 'svelte'
+
+  export let children: SvelteComponent
+  export let variant
+</script>
+
+<button class={$$props.class}>{children}</button>
